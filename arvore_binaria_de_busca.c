d6771e71339ba833b8c5fcf0b9b6b907296afc9f
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-01-28 16:18:35 -0300

    Commit

--- arvore_binaria_de_busca.c
+++ arvore_binaria_de_busca.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Jogador{
+typedef struct Jogador {
     char nome[50];
     int pontos;
     int rebotes;
@@ -11,213 +11,156 @@ typedef struct Jogador{
 
 typedef struct No {
     Jogador jogador;
-    struct No *esquerda;
-    struct No *direita;
+    struct No* esquerda;
+    struct No* direita;
 } No;
 
-No *criar_novo_no(Jogador jogador) {
-    No *novo_no = (No *)malloc(sizeof(No));
-    if (novo_no != NULL) {
-        novo_no->jogador = jogador;
-        novo_no->esquerda = NULL;
-        novo_no->direita = NULL;
-    }
-    return novo_no;
+No* criarNo(Jogador jogador) {
+    No* novoNo = (No*)malloc(sizeof(No));
+    novoNo->jogador = jogador;
+    novoNo->esquerda = NULL;
+    novoNo->direita = NULL;
+    return novoNo;
 }
 
-No *inserir_jogador(No *raiz, Jogador jogador, char chave_de_busca[]) {
+No* inserir(No* raiz, Jogador jogador) {
     if (raiz == NULL) {
-        return criar_novo_no(jogador);
+        return criarNo(jogador);
     }
 
-    int valorAtual = 0;
-    if (strcmp(chave_de_busca, "pontos") == 0) {
-        valorAtual = raiz->jogador.pontos;
-    } else if (strcmp(chave_de_busca, "rebotes") == 0) {
-        valorAtual = raiz->jogador.rebotes;
-    } else if (strcmp(chave_de_busca, "assistencias") == 0) {
-        valorAtual = raiz->jogador.assistencias;
-    }
-
-    int valorJogador = 0;
-    if (strcmp(chave_de_busca, "pontos") == 0) {
-        valorJogador = jogador.pontos;
-    } else if (strcmp(chave_de_busca, "rebotes") == 0) {
-        valorJogador = jogador.rebotes;
-    } else if (strcmp(chave_de_busca, "assistencias") == 0) {
-        valorJogador = jogador.assistencias;
-    }
-
-    if (valorJogador < valorAtual) {
-        raiz->esquerda = inserir_jogador(raiz->esquerda, jogador, chave_de_busca);
-    } else if (valorJogador > valorAtual) {
-        raiz->direita = inserir_jogador(raiz->direita, jogador, chave_de_busca);
+    if (jogador.pontos < raiz->jogador.pontos) {
+        raiz->esquerda = inserir(raiz->esquerda, jogador);
+    } else if (jogador.pontos > raiz->jogador.pontos) {
+        raiz->direita = inserir(raiz->direita, jogador);
     }
 
     return raiz;
 }
 
-No *buscar_jogador(No *raiz, int pontos, char chaveDeBusca[]) {
-    if (raiz == NULL || (strcmp(chaveDeBusca, "pontos") == 0 && raiz->jogador.pontos == pontos) ||
-        (strcmp(chaveDeBusca, "rebotes") == 0 && raiz->jogador.rebotes == pontos) ||
-        (strcmp(chaveDeBusca, "assistencias") == 0 && raiz->jogador.assistencias == pontos)) {
+No* buscar(No* raiz, int pontos) {
+    if (raiz == NULL || raiz->jogador.pontos == pontos) {
         return raiz;
     }
 
-    if (raiz->jogador.pontos > pontos) {
-        return buscar_jogador(raiz->esquerda, pontos, chaveDeBusca);
+    if (pontos < raiz->jogador.pontos) {
+        return buscar(raiz->esquerda, pontos);
     } else {
-        return buscar_jogador(raiz->direita, pontos, chaveDeBusca);
+        return buscar(raiz->direita, pontos);
     }
 }
 
-No *encontrar_no_minimo(No *raiz) {
-    while (raiz->esquerda != NULL) {
-        raiz = raiz->esquerda;
+No* encontrarMinimo(No* no) {
+    while (no->esquerda != NULL) {
+        no = no->esquerda;
     }
-    return raiz;
+    return no;
 }
 
-No *remover_jogador(No *raiz, int pontos, char chave_de_busca[]) {
+No* remover(No* raiz, int pontos) {
     if (raiz == NULL) {
         return raiz;
     }
 
-    int valorAtual = 0;
-    if (strcmp(chave_de_busca, "pontos") == 0) {
-        valorAtual = raiz->jogador.pontos;
-    } else if (strcmp(chave_de_busca, "rebotes") == 0) {
-        valorAtual = raiz->jogador.rebotes;
-    } else if (strcmp(chave_de_busca, "assistencias") == 0) {
-        valorAtual = raiz->jogador.assistencias;
-    }
-
-    if (pontos < valorAtual) {
-        raiz->esquerda = remover_jogador(raiz->esquerda, pontos, chave_de_busca);
-    }
-    else if (pontos > valorAtual) {
-        raiz->direita = remover_jogador(raiz->direita, pontos, chave_de_busca);
-    }
-    else {
+    if (pontos < raiz->jogador.pontos) {
+        raiz->esquerda = remover(raiz->esquerda, pontos);
+    } else if (pontos > raiz->jogador.pontos) {
+        raiz->direita = remover(raiz->direita, pontos);
+    } else {
         if (raiz->esquerda == NULL) {
-            No *temp = raiz->direita;
+            No* temp = raiz->direita;
             free(raiz);
             return temp;
         } else if (raiz->direita == NULL) {
-            No *temp = raiz->esquerda;
+            No* temp = raiz->esquerda;
             free(raiz);
             return temp;
         }
 
-        No *temp = encontrar_no_minimo(raiz->direita);
+        No* temp = encontrarMinimo(raiz->direita);
         raiz->jogador = temp->jogador;
-        raiz->direita = remover_jogador(raiz->direita, temp->jogador.pontos, chave_de_busca);
+        raiz->direita = remover(raiz->direita, temp->jogador.pontos);
     }
+
     return raiz;
 }
 
-void imprimir_jogador_em_ordem(No *raiz, char chave_de_busca[]) {
+void listarEmOrdem(No* raiz) {
     if (raiz != NULL) {
-        imprimir_jogador_em_ordem(raiz->esquerda, chave_de_busca);
-
-        printf("Nome: %s - Pontos: %d, Rebotes: %d, Assistências: %d\n",
-               raiz->jogador.nome, raiz->jogador.pontos,
-               raiz->jogador.rebotes, raiz->jogador.assistencias);
-
-        imprimir_jogador_em_ordem(raiz->direita, chave_de_busca);
+        listarEmOrdem(raiz->esquerda);
+        printf("Nome: %s, Pontos: %d, Rebotes: %d, Assistencias: %d\n",
+               raiz->jogador.nome, raiz->jogador.pontos, raiz->jogador.rebotes, raiz->jogador.assistencias);
+        listarEmOrdem(raiz->direita);
     }
 }
-
-void liberar_arvore(No *raiz) {
-    if (raiz != NULL) {
-        liberar_arvore(raiz->esquerda);
-        liberar_arvore(raiz->direita);
-        free(raiz);
-    }
-}
-
-void menu() {
-    printf("\n......MENU......\n");
+void exibirMenu() {
+    printf("\nMenu:\n");
     printf("\t1 - Inserir jogador\n");
     printf("\t2 - Buscar jogador por pontos\n");
     printf("\t3 - Remover jogador por pontos\n");
     printf("\t4 - Listar jogadores em ordem de pontos\n");
-    printf("\t5 - Sair\n");
-    printf("Escolha uma opção: ");
+    printf("\t0 - Sair\n");
 }
 
 int main() {
-    No *arvore = NULL;
+    No* raiz = NULL;
 
-    while (1) {
-        menu();
-        
-        int opcao;
+    int opcao;
+    do {
+        exibirMenu();
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
         switch (opcao) {
             case 1: {
                 Jogador novoJogador;
-                printf("\nDigite o nome do jogador: ");
+                printf("Informe o nome do jogador: ");
                 scanf("%s", novoJogador.nome);
-                printf("Digite os pontos do jogador: ");
+                printf("Informe a pontuação do jogador: ");
                 scanf("%d", &novoJogador.pontos);
-                printf("Digite os rebotes do jogador: ");
+                printf("Informe o número de rebotes do jogador: ");
                 scanf("%d", &novoJogador.rebotes);
-                printf("Digite as assistências do jogador: ");
+                printf("Informe o número de assistências do jogador: ");
                 scanf("%d", &novoJogador.assistencias);
 
-                char chave[50];
-                printf("Digite a chave de busca: ");
-                scanf("%s", chave);
-
-                arvore = inserir_jogador(arvore, novoJogador, chave);
+                raiz = inserir(raiz, novoJogador);
                 printf("Jogador inserido com sucesso!\n");
                 break;
             }
             case 2: {
-                int pontos;
-                printf("\nDigite os pontos para buscar o jogador: ");
-                scanf("%d", &pontos);
+                int pontosBusca;
+                printf("Informe a pontuação para buscar o jogador: ");
+                scanf("%d", &pontosBusca);
 
-                char chave[50];
-                printf("Digite a chave de busca: ");
-                scanf("%s", chave);
-
-                No *resultado_de_busca = buscar_jogador(arvore, pontos, chave);
-                if (resultado_de_busca != NULL) {
-                    printf("Jogador encontrado: %s\n", resultado_de_busca->jogador.nome);
+                No* jogadorBuscado = buscar(raiz, pontosBusca);
+                if (jogadorBuscado != NULL) {
+                    printf("Jogador encontrado - Nome: %s, Pontos: %d, Rebotes: %d, Assistencias: %d\n",
+                           jogadorBuscado->jogador.nome, jogadorBuscado->jogador.pontos,
+                           jogadorBuscado->jogador.rebotes, jogadorBuscado->jogador.assistencias);
                 } else {
-                    printf("Jogador não encontrado.\n");
+                    printf("Jogador com %d pontos não encontrado.\n", pontosBusca);
                 }
                 break;
             }
             case 3: {
-                int pontos;
-                printf("\nDigite os pontos para remover o jogador: ");
-                scanf("%d", &pontos);
+                int pontosRemocao;
+                printf("Informe a pontuação para remover o jogador: ");
+                scanf("%d", &pontosRemocao);
 
-                char chave[50];
-                printf("Digite a chave de busca: ");
-                scanf("%s", chave);
-
-                arvore = remover_jogador(arvore, pontos, chave);
+                raiz = remover(raiz, pontosRemocao);
                 printf("Jogador removido com sucesso!\n");
                 break;
             }
             case 4:
-                printf("\nLista de jogadores em ordem de pontos:\n");
-                imprimir_jogador_em_ordem(arvore, "pontos");
+                printf("Lista de jogadores em ordem de pontos:\n");
+                listarEmOrdem(raiz);
                 break;
-            case 5:
-                liberar_arvore(arvore);
-                printf("\nPrograma encerrado.\n");
-                return 0;
+            case 0:
+                printf("Saindo do programa. Obrigado!\n");
+                break;
             default:
-                printf("\nOpção inválida. Tente novamente.\n");
-                break;
+                printf("Opcao invalida. Tente novamente.\n");
         }
-    }
+    } while (opcao != 0);
 
     return 0;
 }
